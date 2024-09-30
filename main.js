fe0da84ff612471e8b29d5478842e257b3227bb8
Carlos Henrique <chtsca2001@gmail.com>
2024-09-30 16:28:23 -0300

    Finalizando primeiro-app

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require("node:path");
 
 function createWindow(){
     const win = new BrowserWindow({
         width: 800,
         height: 800,
+        minWidth: 500,
+        minHeight: 500,
+        autoHideMenuBar: true,
+        backgroundColor: "#CCC",
         webPreferences:{
             preload: path.join(__dirname, "preload.js")
         }
     })
 
+    // const child = new BrowserWindow({ 
+    //     parent: win, 
+    //     modal: true, 
+    //     show: false
+    // })
+
+    // child.loadURL("https://sujeitoprogramador.com")
+
+    // child.once("ready-to-show", () => {
+    //     child.show();
+    // })
+
     win.loadFile("index.html");
 }
 
 app.whenReady().then(() => {
+    ipcMain.handle("ping", () => "pong, pong")
+    ipcMain.handle("username", (event, nome) => {
+        console.log("MAIN NOME RECEBIDO: ", nome);
+        return "Bem vindo " + nome
+    })
+
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
---
+++ renderer.js
@@ -0,0 +1,18 @@
+async function teste() {
+    const response = await window.api.ping();
+
+    console.log(response);
+}
+
+teste();
+
+window.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("button")
+    const text = document.getElementById("text")
+
+    button.addEventListener("click", async () => {
+        const username = await window.api.getName("Carlos Henrique");
+        console.log(username);
+        text.textContent = username;
+    });
+});
